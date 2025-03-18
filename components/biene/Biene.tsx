b25c519944ee61dcd30b4a2aea30c3c7147cd1f9
Marko Strassl <69798375+Marko-Mario-Strassl@users.noreply.github.com>
2025-03-18 12:40:44 +0100

    remove

--- components/biene/Biene.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const BieneClient = dynamic(() => import("./BieneClient"), { ssr: false })
-
-export default function BieneWrapper() {
-	return <BieneClient />
-}
--- components/clouds/Clouds.tsx
+++
@@ -1,9 +0,0 @@
-"use client"
-
-import dynamic from "next/dynamic"
-
-const CloudsClient = dynamic(() => import("./CloudsClient"), { ssr: false })
-
-export default function CloudsWrapper() {
-	return <CloudsClient />
-}
